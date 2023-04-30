d3eba97fddf40e4e116e88e4c8bd904a13ea397b
Michal Musil <musilmichal16@gmail.com>
2023-04-30 21:10:41 +0200

    Game end popup window implemented

--- app/src/main/java/cz/mendelu/xmusil5/tictactoe/ui/screens/game_screen/GameScreen.kt
+++ app/src/main/java/cz/mendelu/xmusil5/tictactoe/ui/screens/game_screen/GameScreen.kt
@@ -1,19 +1,35 @@
 package cz.mendelu.xmusil5.tictactoe.ui.screens.game_screen
 
+import android.view.Gravity
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import androidx.compose.ui.window.DialogWindowProvider
 import androidx.hilt.navigation.compose.hiltViewModel
 import cz.mendelu.xmusil5.tictactoe.R
 import cz.mendelu.xmusil5.tictactoe.game.Game
@@ -24,6 +40,9 @@ import cz.mendelu.xmusil5.tictactoe.navigation.INavigationRouter
 import cz.mendelu.xmusil5.tictactoe.ui.components.game_elements.Tile
 import cz.mendelu.xmusil5.tictactoe.ui.components.ui_elements.CustomButton
 import cz.mendelu.xmusil5.tictactoe.ui.theme.red
+import cz.mendelu.xmusil5.tictactoe.ui.theme.shadowColor
+import cz.mendelu.xmusil5.tictactoe.ui.utils.UiConstants
+import cz.mendelu.xmusil5.tictactoe.ui.utils.customShadow
 
 @Composable
 fun GameScreen(
@@ -60,16 +79,37 @@ fun GameScreenContent(
     navigation: INavigationRouter
 ){
     val uiState = viewModel.uiState.collectAsState()
-    val tileBoard = viewModel.boardTiles.collectAsState()
+    val humanPlayerMark = viewModel.humanPlayerMark.value
 
-    val topText = when{
-        uiState.value is GameUiState.HumanPlayerTurn -> stringResource(id = R.string.yourTurn)
-        uiState.value is GameUiState.ComputerPlayerTurn -> stringResource(id = R.string.oponentsTurn)
-        uiState.value is GameUiState.Tie -> stringResource(id = R.string.itsTie)
-        uiState.value is GameUiState.HumanPlayerVictory -> stringResource(id = R.string.youHaveWon)
-        uiState.value is GameUiState.ComputerPlayerVictory -> stringResource(id = R.string.youHaveLost)
-        else -> ""
+    var topText by remember{
+        mutableStateOf("")
     }
+    var currentTurnMark by remember {
+        mutableStateOf<PlayerMark?>(null)
+    }
+    val humanTurnText = stringResource(id = R.string.yourTurn)
+    val computerTurnText = stringResource(id = R.string.oponentsTurn)
+    LaunchedEffect(uiState.value){
+        uiState.value.let {
+            when(it) {
+                is GameUiState.HumanPlayerTurn -> {
+                    topText = humanTurnText
+                    currentTurnMark = humanPlayerMark
+                }
+                is GameUiState.ComputerPlayerTurn -> {
+                    topText = computerTurnText
+                    currentTurnMark = PlayerMark.values().firstOrNull {
+                        it != humanPlayerMark
+                    }
+                }
+                else -> {
+                    topText = ""
+                    currentTurnMark = null
+                }
+            }
+        }
+    }
+    
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -89,19 +129,41 @@ fun GameScreenContent(
                 fontWeight = FontWeight.Bold,
                 color = MaterialTheme.colorScheme.onBackground
             )
-            CustomButton(
-                text = stringResource(id = R.string.concede),
-                backgroundColor = red,
-                textColor = MaterialTheme.colorScheme.background,
-                onClick = {
-                    navigation.toStartupScreen()
-                }
-            )
+            currentTurnMark?.let {
+                Icon(
+                    imageVector = ImageVector.vectorResource(id = it.iconId),
+                    contentDescription = null,
+                    tint = it.color,
+                    modifier = Modifier
+                        .size(30.dp)
+                )
+            }
         }
         
         Spacer(modifier = Modifier.height(50.dp))
         
-        GameBoard(viewModel = viewModel)
+        GameBoard(
+            viewModel = viewModel
+        )
+
+        GameEndPopup(
+            navigation = navigation,
+            viewModel = viewModel
+        )
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        CustomButton(
+            text = stringResource(id = R.string.concede),
+            backgroundColor = red,
+            iconId = R.drawable.ic_close,
+            textColor = MaterialTheme.colorScheme.background,
+            onClick = {
+                navigation.toStartupScreen()
+            }
+        )
+
+        Spacer(modifier = Modifier.height(40.dp))
     }
 }
 
@@ -185,6 +247,126 @@ fun GameBoard(
                     }
                 }
             )
+        }
+    }
+}
+
+
+@Composable
+fun GameEndPopup(
+    navigation: INavigationRouter,
+    viewModel: GameViewModel
+){
+    val uiState = viewModel.uiState.collectAsState()
+
+    var visible by rememberSaveable {
+        mutableStateOf(false)
+    }
+    var mainText by rememberSaveable {
+        mutableStateOf("")
+    }
+
+    val victoryText = stringResource(id = R.string.youHaveWon)
+    val lossText = stringResource(id = R.string.youHaveLost)
+    val tieText = stringResource(id = R.string.itsTie)
+    LaunchedEffect(uiState.value){
+        uiState.value.let {
+            when(it){
+                is GameUiState.ComputerPlayerVictory -> {
+                    visible = true
+                    mainText = lossText
+                }
+                is GameUiState.HumanPlayerVictory -> {
+                    visible = true
+                    mainText = victoryText
+                }
+                is GameUiState.Tie -> {
+                    visible = true
+                    mainText = tieText
+                }
+                else -> {
+                    visible = false
+                    mainText = ""
+                }
+            }
+        }
+    }
+
+    AnimatedVisibility(
+        visible = visible,
+        enter = fadeIn(
+            animationSpec = tween(durationMillis = UiConstants.ANIMATION_DURATION_SHORT)
+        ),
+        exit = fadeOut(
+            animationSpec = tween(durationMillis = UiConstants.ANIMATION_DURATION_SHORT)
+        )
+    ) {
+        val cornerRadius = UiConstants.RADIUS_SMALL
+        Dialog(
+            onDismissRequest = {
+                // Dialog is not cancellable
+            }
+        ) {
+            // setting gravity of the dialog to the bottom of the screen
+            val dialogWindowProvider = LocalView.current.parent as DialogWindowProvider
+            dialogWindowProvider.window.setGravity(Gravity.BOTTOM)
+
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(cornerRadius))
+                        .customShadow(
+                            color = shadowColor,
+                            borderRadius = cornerRadius,
+                            spread = 0.dp,
+                            blurRadius = 5.dp,
+                            offsetY = 2.dp
+                        )
+                        .background(MaterialTheme.colorScheme.surface)
+                        .padding(16.dp)
+                ) {
+                    Text(
+                        text = mainText,
+                        style = MaterialTheme.typography.headlineLarge,
+                        color = MaterialTheme.colorScheme.onBackground,
+                        fontWeight = FontWeight.Bold
+                    )
+
+                    Spacer(modifier = Modifier.height(30.dp))
+
+                    CustomButton(
+                        text = stringResource(id = R.string.playAgain),
+                        backgroundColor = MaterialTheme.colorScheme.secondary,
+                        iconId = R.drawable.ic_play,
+                        textColor = MaterialTheme.colorScheme.onSecondary,
+                        onClick = {
+                            viewModel.uiState.value = GameUiState.Start()
+                        }
+                    )
+
+                    Spacer(modifier = Modifier.height(15.dp))
+
+                    CustomButton(
+                        text = stringResource(id = R.string.backToMenu),
+                        backgroundColor = MaterialTheme.colorScheme.primary,
+                        iconId = R.drawable.ic_menu,
+                        textColor = MaterialTheme.colorScheme.onPrimary,
+                        onClick = {
+                            visible = false
+                            navigation.toStartupScreen()
+                        }
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(30.dp))
+            }
+
         }
     }
 }
